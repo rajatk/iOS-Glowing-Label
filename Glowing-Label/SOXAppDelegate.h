ae77c106a4e6509e42f6f3aadd920cfa2d133f74
Daniele Spagnolo <d.spagnolo@me.com>
2014-02-18 19:00:44 +0000

    Initial Commit

---
+++ Glowing-Label/SOXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SOXAppDelegate.h
+//  Glowing-Label
+//
+//  Created by Daniele Spagnolo on 18/02/2014.
+//  Copyright (c) 2014 SpagOSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SOXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Glowing-LabelTests/Glowing_LabelTests.m
@@ -0,0 +1,34 @@
+//
+//  Glowing_LabelTests.m
+//  Glowing-LabelTests
+//
+//  Created by Daniele Spagnolo on 18/02/2014.
+//  Copyright (c) 2014 SpagOSX. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Glowing_LabelTests : XCTestCase
+
+@end
+
+@implementation Glowing_LabelTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
